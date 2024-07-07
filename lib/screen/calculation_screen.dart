f5d4ba25f0ead70284de475e01956c1d82acbe0e
Krittapas Wongchai <1am8anana@gmail.com>
2024-07-07 14:10:35 +0700

    2

--- lib/screen/calculation_screen.dart
+++ lib/screen/calculation_screen.dart
@@ -10,29 +10,58 @@ class CalculationScreen extends StatefulWidget {
 }
 
 class _CalculationScreenState extends State<CalculationScreen> {
+  double loanAmount = 0, interestRate = 0, result = 0;
+  void calculate() {
+    setState(() {
+      result = loanAmount + (loanAmount * interestRate) / 100;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Calculation')),
       body: Container(
-        child: Column(
-          children: [
-            Text('Amount of money to spend'),
-            Form(
-              child: Column(
-                children: [
-                  TextFormField(),
-                  TextField(),
-                  ElevatedButton.icon(
-                      onPressed: () {},
-                      icon: const Icon(
-                        Icons.calculate,
-                      ),
-                      label: const Text('calculate'))
-                ],
-              ),
-            )
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              Text('Amount of money to spend ${result.toStringAsFixed(2)} THB'),
+              Form(
+                child: Column(
+                  children: [
+                    TextFormField(
+                      keyboardType: TextInputType.number,
+                      decoration:
+                          const InputDecoration(labelText: 'Loan Limits'),
+                      onChanged: (value) {
+                        setState(() {
+                          loanAmount = double.parse(value);
+                        });
+                      },
+                    ),
+                    TextFormField(
+                      keyboardType: TextInputType.number,
+                      decoration:
+                          const InputDecoration(labelText: 'Interest Rate (%)'),
+                      onChanged: (value) {
+                        setState(() {
+                          interestRate = double.parse(value);
+                        });
+                      },
+                    ),
+                    const SizedBox(height: 20),
+                    ElevatedButton.icon(
+                        onPressed: calculate,
+                        icon: const Icon(
+                          Icons.calculate,
+                        ),
+                        label: const Text('calculate'))
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
